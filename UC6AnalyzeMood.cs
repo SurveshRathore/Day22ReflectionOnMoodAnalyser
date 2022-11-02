a25649d2ac0ced377f549eb80679f9271d52a769
SurveshRathore <113424413+SurveshRathore@users.noreply.github.com>
2022-11-02 14:10:16 +0530

    Use Reflection to invoke Method – analyseMood

---
+++ UC6AnalyzeMood.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20MoodAnalyzer
+{
+    public class UC6AnalyzeMood
+    {
+        private string message;
+        public UC6AnalyzeMood(string message)
+        {
+            this.message = message;
+        }
+        public UC6AnalyzeMood()
+        {
+
+        }
+        public string analyseMood()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if (this.message.Contains("sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
+            catch (NullReferenceException obj)
+            {
+                //throw new Exception(obj.Message);
+
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+            catch (MoodAnalyserCustomException ex)
+            {
+                Console.WriteLine("this is our custom exception");
+            }
+            finally
+            {
+                Console.WriteLine("This code will always execute irrespective of exception came or not!!!!");
+            }
+            return null;
+        }
+    }
+}
