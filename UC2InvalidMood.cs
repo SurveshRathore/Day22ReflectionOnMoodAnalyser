1ce4e72ebf937c5f47941c19a2bba7174611bc84
SurveshRathore <113424413+SurveshRathore@users.noreply.github.com>
2022-11-02 14:09:11 +0530

    Handle Exception if User Provides Invalid Mood

---
+++ UC2InvalidMood.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20MoodAnalyzer
+{
+    public class UC2InvalidMood
+    {
+        string message;
+
+        public UC2InvalidMood() { }
+
+        public UC2InvalidMood(string msg)
+        {
+            this.message = msg;
+        }
+        public string analyseMood()
+        {
+            try
+            {
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+            }
+            catch (NullReferenceException obj)
+            {
+                Console.WriteLine("Exception");
+            }
+
+            return "Happy";
+        }
+    }
+}
