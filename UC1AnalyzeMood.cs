a23363bef9070915d2e0a407a39302c22b65806f
SurveshRathore <educatedevilboy@gmail.com>
2022-11-02 14:07:29 +0530

    analyse and respond Happy or Sad Mood

---
+++ UC1AnalyzeMood.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20MoodAnalyzer
+{
+    public class UC1AnalyzeMood
+    {
+        public string analyseMood(string msg)
+        {
+            if (msg.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
