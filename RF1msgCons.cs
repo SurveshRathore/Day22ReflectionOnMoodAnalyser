e2740aadab09fbfa9e9ce99a9a93fa5788f85498
SurveshRathore <113424413+SurveshRathore@users.noreply.github.com>
2022-11-02 14:11:21 +0530

    Refactor code to take mood message in Constructor

---
+++ RF1msgCons.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20MoodAnalyzer
+{
+    public class RF1msgCons
+    {
+        string message;
+
+        public RF1msgCons() { }
+
+        public RF1msgCons(string msg) 
+        { 
+            this.message = msg; 
+        }
+        public string analyseMood()
+        {
+            if (this.message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
