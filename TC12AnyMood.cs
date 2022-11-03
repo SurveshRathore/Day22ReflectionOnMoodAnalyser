f540af737368e8f9ffd88eb8cb8416013d9e7390
SurveshRathore <113424413+SurveshRathore@users.noreply.github.com>
2022-11-03 07:51:10 +0530

    Given “I am in Any Mood” message Return HAPPY

---
+++ TC12AnyMood.cs
@@ -0,0 +1,26 @@
+﻿using Day20MoodAnalyzer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerTest
+{
+    [TestClass]
+    public class TC12AnyMood
+    {
+        [TestMethod]
+        public void GivenAnyReturnHappy()
+        {
+            string expected = "HAPPY";
+            string message = "I am in Any Mood";
+            UC1AnalyzeMood uc1am = new();
+
+            string mood = uc1am.analyseMood(message);
+
+            Assert.AreEqual(expected, mood);
+        }
+        
+    }
+}
