c5a754074814e94ab3ccfc64d8addb7b6f5a41f2
SurveshRathore <113424413+SurveshRathore@users.noreply.github.com>
2022-11-03 07:52:06 +0530

    Given NULL Mood Should Throw MoodAnalysisException

---
+++ TC31NullMood.cs
@@ -0,0 +1,22 @@
+﻿using Day20MoodAnalyzer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerTest
+{
+    public class TC31NullMood
+    {
+        public void GivenNullReturnException()
+        {
+            string expected = null;
+            UC3InvalidMoodInform uc3 = new(null);
+
+            string mood = uc3.analyseMood();
+
+            Assert.AreEqual(expected, mood);
+        }
+    }
+}
